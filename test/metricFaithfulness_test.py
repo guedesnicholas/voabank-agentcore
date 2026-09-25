cd4989e188b161c20110c0d8cd00c61c5ad8ac42
guedesnicholas <nicholasguedes04@gmail.com>
2026-09-25 00:48:37 -0300

    feat: adiociona a métrica Faithfulness

---
+++ test/metricFaithfulness_test.py
@@ -0,0 +1,27 @@
+from deepeval import assert_test
+from deepeval.metrics import FaithfulnessMetric
+from deepeval.test_case import LLMTestCase
+from demos.juiz import obter_juiz
+from dataset.loader_datase import carregar_dataset
+from chatbot import perguntar
+import pytest
+
+
+JUIZ = obter_juiz()
+dataset = carregar_dataset()
+
+
+@pytest.mark.Faithfulness
+class TestFaithfulness:
+
+    @pytest.mark.parametrize("golden", dataset.goldens)
+    def testing(self, golden):
+        actual_output = perguntar(golden.input)
+        test_case = LLMTestCase(
+            input=golden.input,
+            actual_output=actual_output,
+            expected_output=golden.expected_output,
+            retrieval_context=golden.retrieval_context,
+        )
+
+        assert_test(test_case=test_case, metrics=[FaithfulnessMetric(model=JUIZ, threshold=0.8)])
